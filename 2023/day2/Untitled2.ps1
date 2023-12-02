624fd1031c0a3a6770d02bec8e76d30202270866
Drew Boatwright <MdDgDrew@aol.com>
2023-12-02 01:13:51 -0500

    Day 2 2023

---
+++ 2023/day2/Untitled2.ps1
@@ -0,0 +1,46 @@
+﻿#Get the input
+$in = Get-Content .\input.txt
+
+$powerSum = 0;
+
+$in | ForEach-Object {
+    #We need to do some splitting up
+    $res = $_.ToString().Split(":");
+
+    #Now get the subsets within a game
+    $subsets = $res[1].Split(";");
+
+    $maxRed = 0;
+    $maxGreen = 0;
+    $maxBlue = 0;
+
+    #Now I need to get the number and colors within each
+    $subsets | ForEach-Object {
+        #Write-Host $_
+        $cubes = $_.Split(",");
+        $cubes | ForEach-Object {
+            $number = $_ -replace "[^\d]","";
+            $color = $_ -replace "[^a-z]","";
+            if($color -eq "red"){
+                if([int]$number -gt $maxRed){
+                    $maxRed = [int]$number;
+                }
+            }
+            elseif($color -eq "green"){
+                if([int]$number -gt $maxGreen){ 
+                    $maxGreen = [int]$number;
+                }
+            }
+            elseif($color -eq "blue"){
+                if([int]$number -gt $maxBlue){ 
+                    $maxBlue = [int]$number;
+                }
+            }
+        }
+    }
+
+    $powerSum += $maxRed * $maxGreen * $maxBlue
+
+}
+
+Write-Host $powerSum
---
+++ 2023/day2/puzzle1.ps1
@@ -0,0 +1,55 @@
+﻿#Get the input
+$in = Get-Content .\input.txt
+
+$idSum = 0;
+
+$maxRed = 12;
+$maxGreen = 13;
+$maxBlue = 14;
+
+$in | ForEach-Object {
+    #We need to do some splitting up
+    $res = $_.ToString().Split(":");
+    $gameID = $res[0] -replace "[^\d]","";
+
+    #Now get the subsets within a game
+    $subsets = $res[1].Split(";");
+
+    $thisIsAValidGame = $true;
+    #Now I need to get the number and colors within each
+    $subsets | ForEach-Object {
+        #Write-Host $_
+        $cubes = $_.Split(",");
+        $cubes | ForEach-Object {
+            $number = $_ -replace "[^\d]","";
+            $color = $_ -replace "[^a-z]","";
+            #Write-Host $number $color
+            if($color -eq "red"){
+                if([int]$number -gt $maxRed){
+                    #Write-Host "This is no longer a valid game because of red"
+                    $thisIsAValidGame = $false
+                }
+            }
+            elseif($color -eq "green"){
+                if([int]$number -gt $maxGreen){ 
+                    #Write-Host "This is no longer a valid game because of green"
+                    $thisIsAValidGame = $false
+                }
+            }
+            elseif($color -eq "blue"){
+                if([int]$number -gt $maxBlue){ 
+                    #Write-Host "This is no longer a valid game because of blue"
+                    $thisIsAValidGame = $false
+                }
+            }
+        }
+    }
+
+    if($thisIsAValidGame){
+        #Write-Host "This is a valid game"
+        $idSum += $gameID
+    }
+    #Write-Host $gameID
+}
+
+Write-Host $idSum
